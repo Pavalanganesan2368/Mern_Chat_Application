f5fa34cdc37f6e23061bd823db39dd104b71c326
Pavalanganesan2368 <pavalanganesan2368@gmail.com>
2026-03-26 10:52:13 +0530

    Project Completed

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://mern-stack-chat-application-wntn.onrender.com/api" : "http://localhost:5001/api",
   withCredentials: true,
 });
